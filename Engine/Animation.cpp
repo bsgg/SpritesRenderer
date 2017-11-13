5f144c5b83e85b8b1dd4758bcb50679a1a78fcf8
Mart-PC\Mart <beatriz.s.g86@gmail.com>
2017-11-13 09:04:41 +0000

    Include negative effect when  draw a sprite

--- Engine/Animation.cpp
+++ Engine/Animation.cpp
@@ -16,7 +16,22 @@ Animation::Animation(int x, int y, int width, int height, int count,
 
 void Animation::Draw(const Vei2& pos, Graphics& gfx) const
 {
-	gfx.DrawSprite(pos.x, pos.y, frames[iCurFrame], sprite, SpriteEffect::Ghost{ chroma });
+	//gfx.DrawSprite(pos.x, pos.y, frames[iCurFrame], sprite, SpriteEffect::Ghost{ chroma });
+
+	// Negative effect
+	gfx.DrawSprite(pos.x, pos.y, frames[iCurFrame], sprite,
+		// Capturing this, we can get the chroma from Animation clas.
+		[this](Color cSrc, int xDest, int yDest, Graphics& gfx)
+		{
+			if (cSrc != chroma)
+			{
+				// Get the complement colour for each channel. If the channel is very low it will get a very high color 
+				gfx.PutPixel(xDest, yDest,
+					{255u - cSrc.GetR(), 255u - cSrc.GetG(), 255u - cSrc.GetB() }
+				);
+			}
+		}
+	);
 }
 
 void Animation::Draw(const Vei2& pos, Graphics& gfx, const RectI& clip) const
